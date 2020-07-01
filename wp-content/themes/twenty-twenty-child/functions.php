61cefd208a835c342dcd96b04d77db5ba62ad043
rotemyagel <rotemyagel@gmail.com>
2020-07-01 14:57:30 +0300

    Users

--- wp-content/themes/twenty-twenty-child/functions.php
+++ wp-content/themes/twenty-twenty-child/functions.php
@@ -5,3 +5,12 @@ add_action( 'wp_enqueue_scripts', 'tt_child_enqueue_parent_styles' );
 	function tt_child_enqueue_parent_styles() {
 	   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 	}
+	
+
+
+	$user_login = wp_get_current_user()->user_login;
+
+	//Disable wp admin bar for this user by Username
+	if (  $user_login === 'wp-test' ) {
+		add_filter('show_admin_bar', '__return_false');
+	   }
